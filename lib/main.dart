bc3d41dc7b72a99b130d8f2b713cf9036a29a5f8
Gustaf Hultgren <gustaf.hultgren@gmail.com>
2019-09-10 23:58:46 +0200

    add custom font

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(
+                  'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                ),
+              ),
+              Text(
+                'Gustaf Hultgren',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
